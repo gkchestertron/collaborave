2a2c17c5144eee853c6d1c1c34270c68667a247c
gkchesterton <john.fellman@gmail.com>
2013-11-05 21:49:25 -0500

    try heroku ftp again2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,22 +4,9 @@ class StaticPagesController < ApplicationController
       @new_project  = current_user.projects.build(author_id: current_user)
       @user = current_user
       @projects = @user.projects.paginate(page: params[:page])
-    else
-      
-      @user = @project.author
-      @version = Version.new
-      
-      @versions = @project.versions.paginate(page: params[:page])
-      @track = @project.tracks.new
-      @track.project_id = @project.id
-      @tracks = @project.tracks.all
-      @track_urls = []
-      @track_names = []
-      @tracks.each  do |track| 
-        @track_urls << track.path.url if track.path.url != nil
-        @track_names << track.track_name if track.path.url != nil
+    
       end
-    end
+  end
 
   end
 
