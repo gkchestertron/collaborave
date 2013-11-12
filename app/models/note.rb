884982f25101803c3fe10b220f051dde0bb8e851
gkchesterton <john.fellman@gmail.com>
2013-11-12 10:24:13 -0500

    make notes show in ascending order

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
 	belongs_to :project, class_name: "Project"
 	belongs_to :collaborator, class_name: "User"
-	default_scope -> { order('created_at DESC') }
+	default_scope -> { order('created_at ASC') }
 	validates :collaborator_id, presence: true
 	validates :project_id, presence: true
 	validates :content, presence: true
