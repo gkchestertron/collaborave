26fa445e03aafed4baf438b571e388575962c81e
gkchesterton <john.fellman@gmail.com>
2014-01-23 17:07:30 -0800

    changes login routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to root_url
+      if user.name == "guest"
+        redirect_to root_url 
+      else
+        redirect_to user
+      end
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
